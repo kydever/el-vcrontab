f14f521c894da812800ff9e9edfa4aff2701fbe8
李铭昕 <715557344@qq.com>
2022-02-07 13:32:42 +0800

    Update

--- .github/change-version.js
+++ .github/change-version.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 const data = fs.readFileSync("package.json")
 let json = JSON.parse(data);
-json.version = process.argv[2];
+let version = process.argv[2];
+
+json.version = version.replace('refs/tags/v', '');
 
 fs.writeFileSync("package.json", JSON.stringify(json, null, 2));
